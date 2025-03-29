0f3554a19a5313d5a95eb19a437b2deae0adb3c7
Josh-SolisF <josh.solisf@gmail.com>
2025-03-29 17:10:43 -0600

    Implemented movement
    Movement implementations

---
+++ Videogame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed; 
+    public Rigidbody2D rigidbody;
+
+    private Vector2 moveDirection;
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+
+
+    void Update()
+    {
+
+        ProccesInputs();
+        
+    }
+
+
+    void ProccesInputs(){
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+
+    }
+
+    void Move()
+    {
+        
+        rigidbody.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+    }
+
+
+}
