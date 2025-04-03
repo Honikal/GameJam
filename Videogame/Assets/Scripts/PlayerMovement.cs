6ce96089f9771e8b1771eebac3cc337c43976b61
Honikal <81885736+Honikal@users.noreply.github.com>
2025-04-03 14:27:59 -0600

    Modificamos el método por el que manejamos la vida y el timer de cerrar ojos

--- Videogame/Assets/Scripts/PlayerMovement.cs
+++ Videogame/Assets/Scripts/PlayerMovement.cs
@@ -3,34 +3,38 @@ using System;
 using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Movement : MonoBehaviour
 { 
-    [SerializeField] float maxSanity = 100f;  //Manejamos la sanidad o el poder parpadear
+    [SerializeField] float maxClosedEyes = 100f;  //Manejamos la sanidad o el poder parpadear
     [SerializeField] float maxHealth = 100f;  //Manejamos el sistema de salud del jugador, que se reduce cuando observamos al monstruo por mucho tiempo
     //Tiempo que toma que el jugador pierda la cordura completamente
-    [SerializeField] float eyesClosedDuration = 20f;    //Toma 20 segundos para perder la cordura
+    [SerializeField] float eyesClosedCooldown = 3f;    //El jugador puede mantener los ojos cerrados por 5 segundos
+                                                       //Cooldown para determinar el tiempo de ojos cerrados
     [SerializeField] float healthDuration = 5f;    //Toma 5 segundos para perder la salud completa
-    [SerializeField] float healthRecoveryDuration = 30f; //Toma 30 segundos para poder recuperarse la salud de forma completa
 
     //Por si tocara hacer regeneración propia, regeneración por segundo cuando los ojos están abiertos
     //[SerializeField] float eyesOpenRegenRate = 15f;
 
     [Header("Referencias")]
-    public Image SanityBar;
+    public TextMeshProUGUI timerValue;
+    public Image EyesBar;
     public Image HealthBar;
     [SerializeField] private GameObject circulo_0; // The child circle to toggle
     public float moveSpeed;
 
-    private float currentSanity = 100f;
+    private float currentClosedEyes = 100f;
     private float currentHealth = 100f;
-    private float sanityDrainRate;
+    private float closeEyesRate;
     private float healthDrainRate;
-    private float healthRegainRate;
+    private float cooldownTimer = 0f;
     private Rigidbody2D rb;
     private CircleCollider2D visionTrigger;
     private bool isEyesClosed = false;
-    private bool isMonsterInSight = true;
+    private bool isMonsterInSight = false;
+    private bool isCooldownActive = false;
     private Vector2 moveDirection;
 
 
@@ -39,19 +43,19 @@ public class Movement : MonoBehaviour
         //Seteamos los valores iniciales
         rb = GetComponent<Rigidbody2D>();
         visionTrigger = GetComponent<CircleCollider2D>();
-        currentSanity = maxSanity;
         currentHealth = maxHealth;
+        currentClosedEyes = maxClosedEyes;
 
         //Calculamos el rango de pérdida de sanidad basado en el tiempo deseado
-        sanityDrainRate = maxSanity / eyesClosedDuration;
         healthDrainRate = maxHealth / healthDuration;
-        healthRegainRate = maxHealth / healthRecoveryDuration;
+        closeEyesRate = maxClosedEyes / eyesClosedCooldown;
     }
 
     void Update()
     {
         ProccesInputs();
-        //HandleSanity();
+        HandleEyeTimer();
+        HandleCooldown();
         HandleHealth();
         UpdateUI();
     }
@@ -69,72 +73,125 @@ public class Movement : MonoBehaviour
         moveDirection = new Vector2(moveX, moveY).normalized;
 
         //Checamos que se presione el botón space
-        if (Input.GetKeyDown(KeyCode.Space)) ToggleEyes(true);
-        if (Input.GetKeyUp(KeyCode.Space)) ToggleEyes(false);
+        if (Input.GetKeyDown(KeyCode.Space)) TryCloseEyes();
+        if (Input.GetKeyUp(KeyCode.Space)) OpenEyes();
     }
 
-    void ToggleEyes(bool value)
+    void TryCloseEyes()
     {
-        //Activamos el círculo del jugador
-        isEyesClosed = value;
-        circulo_0.SetActive(!value);
-        //Acá también debemos de desactivar o activar el trigger
-        visionTrigger.enabled = !value;
-        Debug.Log(value ? "Ojos cerrados" : "Ojos abiertos");
+        if (!isCooldownActive && currentClosedEyes > 0)
+        {
+            isEyesClosed = true;
+            circulo_0.SetActive(false);
+            visionTrigger.enabled = false;
+            Debug.Log("Ojos cerrados");
+        }
     }
+    void OpenEyes()
+    {
+        //Sé que solo sucederá cuando se suelte el botón pero igual
+        if (isEyesClosed)
+        {
+            isEyesClosed = false;
+            circulo_0.SetActive(true);
+            visionTrigger.enabled = true;
+            StartCooldown();
+            Debug.Log("Ojos abiertos");
+        }
+    }
+
     void Move()
     {
         rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 
-    private void HandleSanity()
-    {
-        if (isEyesClosed)
-        {
-            //Reducimos la sanidad a base de tiempo
-            currentSanity -= sanityDrainRate * Time.deltaTime;
-            Debug.Log($"Perdemos sanidad, sanidad actual: {currentSanity}");
-        }
-        //Nos encargamos de mantener la sanidad en los límites
-        currentSanity = Mathf.Clamp(currentSanity, 0, maxSanity);
-    }
-
     private void HandleHealth()
     {
         //Mediante ésta función, checamos la existencia que hayamos visto al monstruo, y checamos el caso de que tengamos los ojos abiertos
-        if (isMonsterInSight)
+        if (isMonsterInSight && !isEyesClosed)
         {
-            if (!isEyesClosed)
-            {
-                currentHealth -= healthDrainRate * Time.deltaTime;
-                Debug.Log($"Perdemos salud, salud actual: {currentHealth}");
-            }
-            else
-            {
-                currentHealth += healthRegainRate * Time.deltaTime;
-                Debug.Log($"Recuperamos salud, salud actual: {currentHealth}");
-            }
-
+            currentHealth -= healthDrainRate * Time.deltaTime;
+            Debug.Log($"Perdemos salud, salud actual: {currentHealth}");
         }
         //Nos encargamos de mantener la salud en los límites
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
     }
 
-    public void IncreaseSanity(float amount)
+    private void HandleEyeTimer()
     {
-        currentSanity = Mathf.Clamp(currentSanity + amount, 0, maxSanity);
+        //Si mantenemos los ojos cerrados tenemos un cooldown hasta poder volver a abrir los ojos
+        if (isEyesClosed)
+        {
+            currentClosedEyes -= closeEyesRate * Time.deltaTime;
+
+            //Si se llegara al punto en el que la barra de ojos cerrados está afectando, entonces
+            if (currentClosedEyes <= 0)
+            {
+                //Cuando llegue a 0, igual le abriremos los ojos
+                currentClosedEyes = 0;
+                OpenEyes();
+            }
+        }
+        //Mantenemos los estados de ojos cerrados al límite
+        currentClosedEyes = Math.Clamp(currentClosedEyes, 0, maxClosedEyes);
+    }
+
+    private void StartCooldown()
+    {
+        isCooldownActive = true;
+        cooldownTimer = eyesClosedCooldown;
+    }
+    private void HandleCooldown()
+    {
+        if (isCooldownActive)
+        {
+            //Reducimos el timer en base a segundos reales
+            cooldownTimer -= Time.deltaTime;
+            currentClosedEyes += (maxClosedEyes / eyesClosedCooldown) * Time.deltaTime;
+
+            //Si el cooldown está en 0, entonces...
+            if (cooldownTimer <= 0f)
+            {
+                //Podemos cerrar ojos de nuevo
+                isCooldownActive = false;
+                currentClosedEyes = maxClosedEyes;
+            }
+        }
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log($"Health Restored: {currentHealth}");
     }
 
     private void UpdateUI()
     {
         //Solo actualizamos cuando hay cambios
-        float normalizedSanity = currentSanity / maxSanity;
+        float normalizedSight = currentClosedEyes / maxClosedEyes;
         float normalizedHealth = currentHealth / maxHealth;
 
-        Debug.Log($"Tamaño a pintar del campo de sanidad: {normalizedSanity}");
         Debug.Log($"Tamaño a pintar del campo de salud: {normalizedHealth}");
-        SanityBar.fillAmount = normalizedSanity;
+        EyesBar.fillAmount = normalizedSight;
         HealthBar.fillAmount = normalizedHealth;
+
+        //Actualizamos la medida de los ojos
+        EyesUI();
+    }
+
+    private void EyesUI()
+    {
+        //Actualizamos el timer basado en el texto
+        if (isCooldownActive)
+        {
+            timerValue.text = $"Cooldown: {cooldownTimer.ToString("0.0")}s";
+        } else if (isEyesClosed)
+        {
+            timerValue.text = $"Close time: {currentClosedEyes.ToString("0.0")}s";
+        } else
+        {
+            timerValue.text = $"Ready: {currentClosedEyes.ToString("0.0")}s";
+        }
     }
 
     //El monstruo entra en el área de visibilidad del jugador
